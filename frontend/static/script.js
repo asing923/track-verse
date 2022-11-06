f325544fac27387e19b328429f81fc8a107c60fb
asing923 <asing923@uwo.ca>
2022-11-06 12:25:06 -0500

    UI functionality to show genres and fetch tracks

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -6,7 +6,22 @@ nameInput.addEventListener("keypress", function (event) {
     }
 });
 
+var nameInput = document.getElementById("search-by-track-input-two");
+nameInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("search-by-track-btn-two").click();
+    }
+});
+
+window.onload = function () {
+    getAllGenres();
+    loadAllCustomPlaylists('name');
+};
+
 var fetchedTracks = [];
+var customPlaylistTracks = [];
+var sortByShow = false;
 var rootApiPath = '/api'
 
 async function searchByTrackName(sortParam) {
@@ -136,8 +151,166 @@ function resetSearchResult() {
     galleryContainer.innerHTML = "";
 }
 
-function sortTrack(sortParam) {
-    console.log(sortParam)
-    resetSearchResult();
-    searchByTrackName(sortParam);
+function getAllGenres() {
+    const url = rootApiPath + '/listAllGenres';
+    const newUrl = url;
+
+    fetch(
+        newUrl,
+        {
+            headers: { "Content-Type": "application/json" },
+            method: "GET"
+        }
+    )
+        .then(genres => genres.json()
+            .then(genres => {
+                populateGenres(genres);
+            }))
+        .catch(err => {
+            console.log('Error fetching the genres')
+        })
+}
+
+function populateGenres(genres) {
+
+    let genreRootContainer = document.getElementById('genre-container');
+
+    let unorderedList = document.createElement('ul');
+    unorderedList.classList.add("unordered-list-genre");
+
+    genres.forEach(genre => {
+        let genreElement = document.createElement('li');
+        genreElement.classList.add("genre-element");
+
+
+        let label = document.createElement('button');
+        label.onclick = function () {
+            fetchGenreDetails(genre.title)
+        }
+        label.classList.add('genre-button');
+        label.appendChild(document.createTextNode(genre.title));
+        genreElement.appendChild(label);
+        unorderedList.appendChild(genreElement);
+    })
+
+    genreRootContainer.appendChild(unorderedList);
+
+}
+
+function fetchGenreDetails(genreName) {
+
+    const url = rootApiPath + '/genre/' + genreName;
+    const newUrl = url;
+
+
+    fetch(
+        newUrl,
+        {
+            headers: { "Content-Type": "application/json" },
+            method: "GET"
+        }
+    )
+        .then(tracks => tracks.json()
+            .then(tracks => {
+                if (tracks.length) {
+                    populateTracks(tracks);
+                } else {
+                    resetTracksGenre()
+                    alert('No track found in the genre!')
+                }
+
+            }))
+        .catch(err => {
+            console.log('Error fetching the genres')
+        })
+
+}
+
+function populateTracks(trackList) {
+    resetTracksGenre();
+    let maintrackContainer = document.getElementById('genre-tracks');
+    let primaryContainer = document.createElement('div');
+    primaryContainer.id = 'primary-container';
+
+    let label = document.createElement('span');
+    label.appendChild(document.createTextNode('Found matched tracks: ' + trackList.length));
+    primaryContainer.appendChild(label);
+
+    trackList.forEach(track => {
+
+        let trackContainer = document.createElement('div');
+        trackContainer.id = 'track-container';
+        trackContainer.classList.add('track-container')
+
+        let trackName = document.createElement('div');
+        trackName.id = 'track-name';
+        trackName.classList.add('track-name')
+
+        let trackNameLabel = document.createElement('span');
+        trackNameLabel.classList.add('track-name-label')
+        trackNameLabel.appendChild(document.createTextNode('Track Name: '));
+
+        let trackNameValue = document.createElement('span');
+        trackNameValue.classList.add('track-name-label-value')
+        trackNameValue.appendChild(document.createTextNode(track.track_title));
+        trackName.appendChild(trackNameLabel);
+        trackName.appendChild(trackNameValue);
+
+        let trackArtist = document.createElement('div');
+        trackArtist.id = 'track-artist';
+        trackArtist.classList.add('track-artist')
+
+        let artistNameLabel = document.createElement('span');
+        artistNameLabel.classList.add('artist-name-label')
+        artistNameLabel.appendChild(document.createTextNode('Artist Name: '));
+        let artistNameValue = document.createElement('span');
+        artistNameValue.classList.add('artist-name-label-value')
+        artistNameValue.appendChild(document.createTextNode(track.artist_name));
+        trackArtist.appendChild(artistNameLabel);
+        trackArtist.appendChild(artistNameValue);
+
+        let trackAlbum = document.createElement('div');
+        trackAlbum.id = 'track-album';
+        trackAlbum.classList.add('track-album')
+
+        let albumNameLabel = document.createElement('span');
+        albumNameLabel.classList.add('album-name-label')
+        albumNameLabel.appendChild(document.createTextNode('Album Name: '));
+        let albumNameValue = document.createElement('span');
+        albumNameValue.classList.add('album-name-label-value')
+        albumNameValue.appendChild(document.createTextNode(track.album_title));
+        trackAlbum.appendChild(albumNameLabel);
+        trackAlbum.appendChild(albumNameValue);
+
+        let trackDuration = document.createElement('div');
+        trackDuration.id = 'track-duration';
+        trackDuration.classList.add('track-duration')
+
+        let durationNameLabel = document.createElement('span');
+        durationNameLabel.classList.add('duration-name-label')
+        durationNameLabel.appendChild(document.createTextNode('Duration: '));
+        let durationNameValue = document.createElement('span');
+        durationNameValue.classList.add('duration-name-label-value')
+        durationNameValue.appendChild(document.createTextNode(track.track_duration));
+        trackDuration.appendChild(durationNameLabel);
+        trackDuration.appendChild(durationNameValue);
+
+        trackContainer.appendChild(trackName);
+        trackContainer.appendChild(trackArtist);
+        trackContainer.appendChild(trackAlbum);
+        trackContainer.appendChild(trackDuration);
+        primaryContainer.appendChild(trackContainer);
+    })
+
+    maintrackContainer.appendChild(primaryContainer);
+
+}
+
+function resetTracksGenre() {
+    let genreTracksContainer = document.getElementById('genre-tracks')
+    let trackSection = document.getElementById('primary-container');
+    if (trackSection) {
+        trackSection.appendChild(document.createTextNode(""));
+        genreTracksContainer.removeChild(trackSection);
+    }
 }
